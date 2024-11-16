7d8316c45b33b5126830665a5f394a461b02822a
Hyeonjin Jeong <161837399+dev-jinius@users.noreply.github.com>
2024-11-16 15:07:41 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,20 +6,34 @@ import java.util.*;
 */
 class Solution {
     public int solution(String[][] clothes) {
+        //int answer = 1;
+        //
+        //Map<String, Integer> map = new HashMap<>();
+        //
+        //for (int i = 0; i < clothes.length; i++) {
+        //    if (map.containsKey(clothes[i][1])) {
+        //        map.compute(clothes[i][1], (key, value) -> value + 1);
+        //    } else {
+        //        map.put(clothes[i][1], 1);
+        //    }
+        //}
+        //
+        //for (String key : map.keySet()) {
+        //    answer *= (map.get(key)+1);
+        //}
+        //answer -= 1;
+        //
+        //return answer;
         int answer = 1;
         
         Map<String, Integer> map = new HashMap<>();
-        
         for (int i = 0; i < clothes.length; i++) {
-            if (map.containsKey(clothes[i][1])) {
-                map.compute(clothes[i][1], (key, value) -> value + 1);
-            } else {
-                map.put(clothes[i][1], 1);
-            }
+            String key = clothes[i][1];
+            map.put(key, map.getOrDefault(key, 0)+1);
         }
         
         for (String key : map.keySet()) {
-            answer *= (map.get(key)+1);
+            answer *= map.get(key)+1;
         }
         answer -= 1;
         
