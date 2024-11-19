486ce0d58658c43872644c79c5779608fd8583b2
Iris <161837399+dev-jinius@users.noreply.github.com>
2024-11-19 13:07:08 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,15 @@
 import java.util.List;
 import java.util.ArrayList;
 
-class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        
-//         yellow = 2 -> 1x2
-//         yellow = 1 -> 1x1
 //         yellow = 24 -> 1x24 2x12 3x8 4x6
-        
 //         brown = (24+2)*(1+2) = 26 x 3 = 78-24=54
 //         brown = (12+2)x(2+2) = 14 x 4 = 56-24=32
 //         brwon = (8+2)x(3+2) = 10 x 5 = 50-24=26
 //         brwon = (6+2)x(4+2) = 48-24=24
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
         List<Integer> list = new ArrayList<>();
         for (int i = 1; i*i <= yellow; i++) {
             if (yellow%i == 0)
