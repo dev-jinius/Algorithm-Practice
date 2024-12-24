9aa46a2b0c6e555775a56370f56b818b755a2c37
Iris <161837399+dev-jinius@users.noreply.github.com>
2024-12-24 19:11:01 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -10,12 +10,10 @@ class Solution {
         char prev = ' ';
 
         for (int i = 0; i < words.length; i++) {
-            if (i > 0 && words[i].charAt(0) != prev) 
+            if ((i > 0 && words[i].charAt(0) != prev) || record.contains(words[i]))
                 return new int[] {(i+1) % n == 0 ? n : (i+1) % n, count};
             
             prev = words[i].charAt(words[i].length()-1);
-            if (record.contains(words[i]))
-                return new int[] {(i+1) % n == 0 ? n : (i+1) % n, count};
             
             record.add(words[i]);
             if ((i+1) % n == 0) count++;
