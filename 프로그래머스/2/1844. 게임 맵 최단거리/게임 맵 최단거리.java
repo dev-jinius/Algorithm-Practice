fd1e7fa0f4e050772b0cb79e548f8371ccb1eb0c
Iris <161837399+dev-jinius@users.noreply.github.com>
2025-07-13 20:16:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.84 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,40 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int[] dx = {1, -1, 0, 0}; 		// 동서남북 방향 벡터
-        int[] dy = {0, 0, 1, -1}; 		// 동서남북 방향 벡터
-        int xMax = maps[0].length - 1; 	// x 좌표의 최대값
-        int yMax = maps.length - 1; 	// y 좌표의 최대값
+        int cnt = 1;
         
-        boolean[][] visit = new boolean[yMax + 1][xMax + 1]; // 방문 체크 배열
-        Queue<int[]> queue = new LinkedList<>(); // BFS를 위한 큐
-        queue.add(new int[] {0, 0, 1}); // 시작점 (0, 0)과 시작 거리 1을 큐에 추가
-        visit[0][0] = true; // 시작점 방문 체크
+        int row = maps.length;
+        int col = maps[0].length;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        boolean[][] visited = new boolean[row][col];
+        int[][] dist = new int[row][col];
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0,0});
+        visited[0][0] = true;
+        dist[0][0] = 1;
         
-        while (!queue.isEmpty()) { 			// 큐가 비어있지 않으면 반복
-            int[] current = queue.poll(); 	// 큐에서 현재 노드를 꺼냄
-            int x = current[0]; 			// 현재 x 좌표
-            int y = current[1]; 			// 현재 y 좌표
-            int dist = current[2]; 			// 현재까지의 거리
-            
-            if (x == xMax && y == yMax) { 	// 목표 지점에 도달하면
-                return dist; 				// 현재까지의 거리를 반환
-            }
-            
-            for (int i = 0; i < 4; i++) { 	// 동서남북 네 방향으로 탐색
-                int nx = x + dx[i]; 		// 새로운 x 좌표
-                int ny = y + dy[i]; 		// 새로운 y 좌표
-                
-                // 탐색 종료 조건(맵 벗어남, 벽, 재방문)
-                if (nx < 0 || ny < 0 || nx > xMax || ny > yMax) continue;
-                if (maps[ny][nx] == 0) continue;
-                if (visit[ny][nx]) continue;
-                
-                visit[ny][nx] = true;					// 방문 체크
-                queue.add(new int[] {nx, ny, dist+1}); 	// 현재 좌표 큐에 추가
+        while(!queue.isEmpty()) {
+            int[] player = queue.poll();
+            int cx = player[0];
+            int cy = player[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && nx < row && ny >= 0 && ny < col) {
+                    if (!visited[nx][ny] && maps[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        dist[nx][ny] = dist[cx][cy] + 1;
+                        queue.offer(new int[]{nx, ny});
+                    }
+                }
             }
         }
         
-        return -1; // 큐를 다 돌고 목표 지점에 도달할 수 없는 경우
+        return dist[row-1][col-1] <= 1 ? -1 : dist[row-1][col-1];
     }
 }
